7f4c3597e07ce6fac76eaf69d15ab96c65648c53
katsu8 <katsumi.ishihara08@gmail.com>
2024-08-11 16:25:30 +0200

    adding imgs to favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Taiyaki in Soest",
   description: "Taiyaki in Soest",
+  icons: {
+    icon: "/assets/favicon.ico",
+  },
 };
 
 export default function RootLayout({
