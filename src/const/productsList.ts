2f786a44ac6fbdce3fd97f154f8e451b681d8fd1
katsu8 <katsumi.ishihara08@gmail.com>
2024-08-12 10:56:42 +0200

    small fix

--- src/const/productsList.ts
+++ src/const/productsList.ts
@@ -7,18 +7,18 @@ export const menuItems: Product[] = [
   { name: "Taiyaki mit Anko (süße rote Bohnenpaste)", price: "4,50€" },
   { name: "Taiyaki mit Vanillepudding", price: "4,50€" },
   {
-    name: "Taiyaki mit Schokolade",
-    remarks: "Bald verfügbar, kontaktieren Sie uns für Details",
-    price: "4,50€",
-  },
-  {
     name: "Taiyaki mit Nutella Creme",
     remarks: "Bald verfügbar, kontaktieren Sie uns für Details",
     price: "4,50€",
   },
-  {
-    name: "Taiyaki mit Schoko und Erdbeere",
-    remarks: "Bald verfügbar, kontaktieren Sie uns für Details",
-    price: "5,50€",
-  },
+  // {
+  //   name: "Taiyaki mit Schokolade",
+  //   remarks: "Bald verfügbar, kontaktieren Sie uns für Details",
+  //   price: "4,50€",
+  // },
+  // {
+  //   name: "Taiyaki mit Schoko und Erdbeere",
+  //   remarks: "Bald verfügbar, kontaktieren Sie uns für Details",
+  //   price: "5,50€",
+  // },
 ];
